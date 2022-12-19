f31e4c1e5e7892c59c01de754bf12a68c1453c21
TRMSC <86586942+TRMSC@users.noreply.github.com>
2022-12-19 19:37:22 +0100

    add counter and return

--- main.js
+++ main.js
@@ -2,17 +2,19 @@
  * Generate text
  * 
  * @function generateText
- * @param {string} text
+ * @param {string} text Content to prepare for the output
+ * @param {number} count Spacing of the seperated words
+ * @return output Mdified text
  * 
 */
-generateText = (text) => { 
+generateText = (text, count) => { 
 const subindex = text.search(/[.?!]/);
 let output = text.substring(0, subindex + 1);
 let sentences = text.substring(subindex + 1);
 let words = sentences.match(/[\w']+|[^\s\w']+/g);
 
 for (let j = 0; j < words.length; j++) {
-  if (j % 3 == 2 && words[j].match(/^[\w']+$/)) {
+  if (j % count == count - 1 && words[j].match(/^[\w']+$/)) {
     if (words[j].length % 2 == 1) {
       let halfLength = Math.ceil(words[j].length / 2);
       let firstHalf = words[j].substring(0, halfLength);
@@ -30,4 +32,5 @@ for (let j = 0; j < words.length; j++) {
   }
 }
 console.log(output);
+return output;
 };

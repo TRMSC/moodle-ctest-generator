eac98a1547ade9ec45d7d44842aaa048cd51452d
TRMSC <86586942+TRMSC@users.noreply.github.com>
2022-12-19 19:43:52 +0100

    clean code

--- main.js
+++ main.js
@@ -8,29 +8,29 @@
  * 
 */
 generateText = (text, count) => { 
-const subindex = text.search(/[.?!]/);
-let output = text.substring(0, subindex + 1);
-let sentences = text.substring(subindex + 1);
-let words = sentences.match(/[\w']+|[^\s\w']+/g);
+  const subindex = text.search(/[.?!]/);
+  let output = text.substring(0, subindex + 1);
+  let sentences = text.substring(subindex + 1);
+  let words = sentences.match(/[\w']+|[^\s\w']+/g);
 
-for (let j = 0; j < words.length; j++) {
-  if (j % count == count - 1 && words[j].match(/^[\w']+$/)) {
-    if (words[j].length % 2 == 1) {
-      let halfLength = Math.ceil(words[j].length / 2);
-      let firstHalf = words[j].substring(0, halfLength);
-      let secondHalf = words[j].substring(halfLength + 1);
-      output += firstHalf + "{1:SHORTANSWER:=" + secondHalf + "} ";
+  for (let j = 0; j < words.length; j++) {
+    if (j % count == count - 1 && words[j].match(/^[\w']+$/)) {
+      if (words[j].length % 2 == 1) {
+        let halfLength = Math.ceil(words[j].length / 2);
+        let firstHalf = words[j].substring(0, halfLength);
+        let secondHalf = words[j].substring(halfLength + 1);
+        output += firstHalf + "{1:SHORTANSWER:=" + secondHalf + "} ";
+      } else {
+        let halfLength = words[j].length / 2;
+        let firstHalf = words[j].substring(0, halfLength);
+        let secondHalf = words[j].substring(halfLength);
+        output += firstHalf + "{1:SHORTANSWER:=" + secondHalf + "} ";
+      }
     } else {
-      let halfLength = words[j].length / 2;
-      let firstHalf = words[j].substring(0, halfLength);
-      let secondHalf = words[j].substring(halfLength);
-      output += firstHalf + "{1:SHORTANSWER:=" + secondHalf + "} ";
+      output += words[j] + " ";
+      output = output.replace(/\s+([.?!])/g, "$1");
     }
-  } else {
-    output += words[j] + " ";
-    output = output.replace(/\s+([.?!])/g, "$1");
   }
-}
-console.log(output);
-return output;
+  console.log(output);
+  return output;
 };

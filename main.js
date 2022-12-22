6f8b6e3742f93e4fc1283156d0442b1d07834daf
TRMSC <solidware@web.de>
2022-12-22 12:20:49 +0100

    revise and add event listeners

--- main.js
+++ main.js
@@ -23,7 +23,8 @@ window.onload = () => {
   document.getElementById('autogenerate').addEventListener('click', checkToggle);
   document.getElementById('generate-auto').addEventListener('click', handleAuto);
   document.getElementById('generate-manual').addEventListener('click', handleManual);
-  document.getElementById('auto-close').addEventListener('click', closeMsg('auto-error'));
+  document.getElementById('auto-close').addEventListener('click', function() { closeMsg('auto-error'); });
+  document.getElementById('manual-close').addEventListener('click', function() { closeMsg('manual-error'); });
 
 };
 
